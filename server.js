362bb60316b6f397c6f31cd9a25cdee14434ca87
Rellyson <rellysondouglas2015@gmail.com>
2020-06-28 17:25:48 -0300

    Refraathing: changing to dynamic data

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const nunjuck = require('nunjucks')
 
+const data = require('./data')
+
 const server = express()
 
 nunjuck.configure({
@@ -9,13 +11,23 @@ nunjuck.configure({
     autoescape: false
 })
 
-server.set("view engine", "html")
+server.set("view engine", "njk")
 
 server.use(express.static('public'))
 
 server.get('/', (req, res) => {
-    return res.render('index')
+    return res.render('home', {recipes: data})
 })
+
+server.get('/about', (req, res) => {
+    return res.render('about')
+})
+
+server.get('/recipes', (req, res) => {
+    return res.render('recipes',  {recipes: data})
+})
+
+server.get('/recipes/:index')
 
 server.listen(5000, function () {
     console.log('server is running')
