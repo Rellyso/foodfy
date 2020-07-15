43a5b2ec8b220a54e0292140fd17b0552c9fc9cc
Rellyson <rellysondouglas2015@gmail.com>
2020-07-15 12:15:11 -0300

    added new functions on routes

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,9 +1,35 @@
-const data = require('../data')
+const fs = require('fs')
+const data = require('../data.json')
 
 exports.index = (req, res) => {
-    return res.render('admin/index', {recipes: data})
+    return res.render('admin/index', {recipes: data.recipes})
 }
 
 exports.create = (req, res) => {
     return res.render('admin/create')
+}
+
+exports.post = (req, res) => {
+    const keys = Object.keys(req.body)
+
+
+    for (let key of keys) {
+        if (req.body[key] == "") return res.send('Please fill in all fields.')
+    }
+
+    let { image, title, author, ingredients, preparation, information } = req.body
+
+    data.recipes.push({
+        image,
+        title,
+        author,
+        ingredients,
+        preparation,
+        information
+    })
+    fs.writeFile('data.json', JSON.stringify(data, null, 4), function (err) {
+        if (err) return res.send('Write file error.')
+    })
+
+    return res.redirect('/admin/recipes')
 }
