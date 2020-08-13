de56283811664218a576bb06a15059ae137be250
Rellyson <rellysondouglas2015@gmail.com>
2020-08-13 11:14:34 -0300

    Getting new routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const main = require('./app/controllers/main')
-const admin = require('./app/controllers/admin')
+const admin = require('./app/controllers/recipes')
+const chefs = require('./app/controllers/chefs')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
@@ -20,6 +21,15 @@ routes.get('/admin/recipes/:id/edit', admin.edit)
 routes.post('/admin/recipes', admin.post)
 routes.put('/admin/recipes', admin.put)
 routes.delete('/admin/recipes', admin.delete)
+
+routes.get('/admin/chefs', chefs.index)
+routes.get('/admin/chefs/create', chefs.create)
+routes.get('/admin/chefs/:id', chefs.show)
+routes.get('/admin/chefs/:id/edit', chefs.edit)
+
+routes.post('/admin/chefs', chefs.post)
+routes.put('/admin/chefs', chefs.put)
+routes.delete('/admin/chefs', chefs.delete)
 
 
 module.exports = routes
