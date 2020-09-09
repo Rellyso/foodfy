d731a4a9cfc2d2e21284db9c34e49add03fc2bb2
Rellyson <rellysondouglas2015@gmail.com>
2020-09-09 11:03:08 -0300

    Updating...

---
+++ foodfy.sql
@@ -0,0 +1,26 @@
+CREATE TABLE "files" (
+  "id" serial PRIMARY KEY,
+  "name" text,
+  "path" text NOT NULL
+);
+
+CREATE TABLE "recipe_files" (
+  "id" serial PRIMARY KEY,
+  "recipe_id" int NOT NULL,
+  "file_id" int NOT NULL
+);
+
+CREATE TABLE "recipes" (
+  "id" serial PRIMARY KEY,
+  "chef_id" int NOT NULL,
+  "image" text,
+  "title" text,
+  "ingredients" text[],
+  "preparation" text[],
+  "information" text,
+  "created_at" timestamp DEFAULT (now())
+);
+
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
+
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
--- src/server.js
+++ src/server.js
@@ -1,16 +1,10 @@
 const express = require('express')
-const nunjuck = require('nunjucks')
+const nunjucks = require('nunjucks')
 const routes = require('./routes')
 const methodOverride = require('method-override')
 
 const server = express()
 
-
-nunjuck.configure("src/app/views", {
-    express: server,
-    noCache: true,
-    autoescape: false
-})
 
 server.use(express.urlencoded({ extended: true }))
 server.use(express.static('public'))
@@ -19,6 +13,14 @@ server.use(methodOverride('_method'))
 server.use(routes)
 
 server.set("view engine", "njk")
+
+
+nunjucks.configure("src/app/views", {
+    express: server,
+    autoescape: false,
+    noCache: true,
+})
+
 
 server.listen(5000, function () {
     console.log('server is running')
