05ad8c3236677696bef1dc82cab659a4c908ba0a
Rellyson <rellysondouglas2015@gmail.com>
2020-09-10 09:47:41 -0300

    Refraathing for optmize | Adding async functions | Uptading to receive .map function where was a for loop

--- src/app/controllers/recipes.js
+++ src/app/controllers/recipes.js
@@ -1,102 +1,102 @@
 const Recipe = require('../models/Recipe')
 
-exports.index = (req, res) => {
-    Recipe.selectAllWithChefNames((recipes) => {
-        return res.render('admin/recipes/index', { recipes })
-    })
-}
-
-exports.create = (req, res) => {
-    Recipe.selectChefOptions((options) => {
-        return res.render('admin/recipes/create', { options })
-    })
-}
-
-exports.post = (req, res) => {
-    const { ingredients, preparation } = req.body
-
-    let newIngredients = [],
-        newPreparation = []
-
-    for (let ingredient of ingredients) {
-        if (ingredient != "") {
-            newIngredients.push(ingredient)
-        }
-    }
-
-    for (let prep of preparation) {
-        if (prep != "") {
-            newPreparation.push(prep)
-        }
-    }
-
-    const data = {
-        ...req.body,
-        ingredients: newIngredients,
-        preparation: newPreparation
-    }
-
-    Recipe.create(data, (recipe) => {
-        return res.redirect(`/admin/recipes/${recipe.id}`)
-    })
-}
-
-exports.show = (req, res) => {
-    const { id } = req.params
-
-    Recipe.find(id, (recipe) => {
-        return res.render('admin/recipes/show', { recipe })
-    })
-}
-
-exports.edit = (req, res) => {
-    const { id } = req.params
-
-    Recipe.find(id, (recipe) => {
-        Recipe.selectChefOptions((options) => {
-            res.render('admin/recipes/edit', { recipe, options })
+module.exports = {
+    index(req, res) {
+        Recipe.selectAllWithChefNames((recipes) => {
+            return res.render('admin/recipes/index', { recipes })
         })
-    })
-}
+    },
+    
+    create(req, res) {
+        Recipe.selectChefOptions((options) => {
+            return res.render('admin/recipes/create', { options })
+        })
+    },
+    
+    async post(req, res) {
+        const { ingredients, preparation } = req.body
+    
+        let newIngredients = [],
+            newPreparation = []
 
-exports.put = (req, res) => {
-    const { ingredients, preparation } = req.body
-    let newIngredients = [],
-        newPreparation = []
+        ingredients.map(ingredient => {
+            if (ingredient != "" || ingredient != 0)
+                newIngredients.push(ingredient)
+        })
 
-    for (let ingredient of ingredients) {
-        if (ingredient === "") {
-
-        } else {
-            newIngredients.push(ingredient)
+        preparation.map(prep => {
+            if (prep != "" || prep != 0)
+                newPreparation.push(prep)
+        })
+    
+        const data = {
+            ...req.body,
+            ingredients: newIngredients,
+            preparation: newPreparation
         }
-    }
 
-    for (let prep of preparation) {
-        if (prep == "") {
+        let results = await Recipe.create(data)
+        const recipeId = results.rows[0].id
 
-        } else {
-            newPreparation.push(prep)
+    },
+    
+    show(req, res) {
+        const { id } = req.params
+    
+        Recipe.find(id, (recipe) => {
+            return res.render('admin/recipes/show', { recipe })
+        })
+    },
+    
+    edit(req, res) {
+        const { id } = req.params
+    
+        Recipe.find(id, (recipe) => {
+            Recipe.selectChefOptions((options) => {
+                res.render('admin/recipes/edit', { recipe, options })
+            })
+        })
+    },
+
+    put(req, res) {
+        const { ingredients, preparation } = req.body
+        let newIngredients = [],
+            newPreparation = []
+    
+        for (let ingredient of ingredients) {
+            if (ingredient === "") {
+    
+            } else {
+                newIngredients.push(ingredient)
+            }
         }
-    }
-
-    const paramsBody = {
-        ...req.body,
-        id: parseInt(req.body.id, 10),
-        chef_id: parseInt(req.body.chef_id, 10),
-        ingredients: newIngredients,
-        preparation: newPreparation,
-    }
-
-    Recipe.update(paramsBody, (recipe) => {
-        return res.redirect(`/admin/recipes/${recipe.id}`)
-    })
-}
-
-exports.delete = (req, res) => {
-    const { id } = req.body
-
-    Recipe.delete(id, () => {
-        res.redirect('/admin/recipes')
-    })
+    
+        for (let prep of preparation) {
+            if (prep == "") {
+    
+            } else {
+                newPreparation.push(prep)
+            }
+        }
+    
+        const paramsBody = {
+            ...req.body,
+            id: parseInt(req.body.id, 10),
+            chef_id: parseInt(req.body.chef_id, 10),
+            ingredients: newIngredients,
+            preparation: newPreparation,
+        }
+    
+        Recipe.update(paramsBody, (recipe) => {
+            return res.redirect(`/admin/recipes/${recipe.id}`)
+        })
+    },
+    
+    delete(req, res) {
+        const { id } = req.body
+    
+        Recipe.delete(id, () => {
+            res.redirect('/admin/recipes')
+        })
+    },
 }
