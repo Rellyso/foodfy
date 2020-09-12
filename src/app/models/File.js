c770b00bb62109d8514bfcc51c494fd187e6681a
Rellyson <rellysondouglas2015@gmail.com>
2020-09-12 10:57:46 -0300

    Creating File query function

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -1,22 +1,18 @@
 const db = require('../../config/db')
 
 module.exports = {
-    create({file, path, product_id}) {
+    create({filename, path}) {
         const query = `
             INSERT INTO recipes (
                 name,
                 path,
             ) VALUES ($1, $2)
-            RETURNING id `
+            RETURNING id 
+            INSERT INTO recipe_files ()` // continuar
+            
 
         const values = [
-            params.chef_id,
-            params.image,
-            params.title,
-            params.ingredients,
-            params.preparation,
-            params.information,
-            date(Date.now()).iso,
+            file
         ]
 
         return db.query(query, values)
