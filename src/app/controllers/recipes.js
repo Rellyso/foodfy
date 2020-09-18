c2eb9d45e611d4f589e2b6c86437461bfe5616a7
Rellyson <rellysondouglas2015@gmail.com>
2020-09-18 14:20:36 -0300

    getting edit, index and put routes' functions | updating to receive new conditions | updating to show images on index page

--- src/app/controllers/recipes.js
+++ src/app/controllers/recipes.js
@@ -3,22 +3,49 @@ const File = require('../models/File')
 
 module.exports = {
     async index(req, res) {
-        let results = await Recipe.selectAllWithChefNames()
+        let results = await Recipe.selectAllWithChefNamesAndFiles()
         const recipes = results.rows
 
+        let lastId = 0
+        let filteredRecipes = []
+        recipes.forEach(recipe => {
+
+            
+            if (recipe.id !== lastId) {
+                filteredRecipes.push(recipe)
+            }
+            if (recipe.path != null) {
+                recipe.src = `${req.protocol}://${req.headers.host}${recipe.path.replace('public', '')}`
+            }
+
+            lastId = recipe.id
+        })
         
-        return res.render('admin/recipes/index', { recipes })
+        return res.render('admin/recipes/index', { recipes: filteredRecipes })
 
     },
 
-    create(req, res) {
-        Recipe.selectChefOptions((options) => {
-            return res.render('admin/recipes/create', { options })
-        })
+    async create(req, res) {
+        let results = await Recipe.selectChefOptions()
+        const options = results.rows
+
+        return res.render('admin/recipes/create', { options })
     },
 
     async post(req, res) {
         const { ingredients, preparation } = req.body
+
+        const keys = new Object.keys(req.body)
+
+        for (let key of keys) {
+            if (req.body[key] == "" && key != "removed_files") {
+                return res.send('Please fill in all the fields.')
+            }
+        }
+
+        if (req.files.length == 0) {
+            return res.send("Please send at least one image.")
+        }
 
         let newIngredients = [],
             newPreparation = []
@@ -39,9 +66,6 @@ module.exports = {
             preparation: newPreparation
         }
 
-        if (req.files == 0) {
-            res.send('Please send at least an image.')
-        }
         try {
             let results = await Recipe.create(data)
             const recipeId = results.rows[0].id
@@ -76,30 +100,56 @@ module.exports = {
         }
     },
 
-    edit(req, res) {
+    async edit(req, res) {
         const { id } = req.params
 
-        Recipe.find(id, (recipe) => {
-            Recipe.selectChefOptions((options) => {
-                res.render('admin/recipes/edit', { recipe, options })
-            })
+        let results = await Recipe.find(id)
+        const recipe = results.rows[0]
+
+        results = await Recipe.selectChefOptions()
+        const options = results.rows
+
+        results = await File.getFilesByRecipeId(id)
+        let files = results.rows
+
+        files.map(file => {
+            file.src = `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
         })
+        
+        res.render('admin/recipes/edit', { recipe, options, files })
     },
 
     async put(req, res) {
+        // teste de campos preenchidos
+        const keys = new Object.keys(req.body)
+        for (let key of keys) {
+            if (req.body[key] == "" && key != "removed_files") {
+                return res.send('Please fill in all the fields.')
+            }
+        }
+        
+        // teste de envio de pelo menos uma imagem
+        if (req.files.length == 0) {
+            return res.send("Please send at least one image.")
+        }
+
+
         const { ingredients, preparation } = req.body
         let newIngredients = [],
             newPreparation = []
 
-        newIngredients = ingredients.map(ingredient => {
-            if (ingredient != "") true
+        ingredients.map(ingredient => {
+            if (ingredient != "" || ingredient != 0)
+                newIngredients.push(ingredient)
         })
 
-        newPreparation = preparation.map(prep => {
-            if (prep != "") true
+        preparation.map(prep => {
+            if (prep != "" || prep != 0)
+                newPreparation.push(prep)
         })
 
-        const paramsBody = {
+
+        const data = {
             ...req.body,
             id: parseInt(req.body.id, 10),
             chef_id: parseInt(req.body.chef_id, 10),
@@ -110,8 +160,11 @@ module.exports = {
         if (req.files == 0) {
             res.send('Please send at least an image.')
         }
+
+        // criar lógica de removed_files para excluir arquivos de imagem do files
+
         try {
-            let results = await Recipe.update(paramsBody)
+            let results = await Recipe.update(data)
             const recipeId = results.rows[0].id
 
 
