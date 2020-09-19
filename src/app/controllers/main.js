e33a29a9eeb812106bb591085cda4ee15a26974b
Rellyson <rellysondouglas2015@gmail.com>
2020-09-19 16:52:12 -0300

    updating to receive uploaded images

--- src/app/controllers/main.js
+++ src/app/controllers/main.js
@@ -1,40 +1,57 @@
 const Recipe = require("../models/Recipe")
 const Chef = require("../models/Chef")
 
-exports.index = (req, res) => {
-    Recipe.selectAllWithChefNames((recipes) => {
+module.exports = {
+    async index(req, res) {
+        let results = await Recipe.selectAllWithChefNamesAndFiles()
+        const recipes = results.rows
+
+        let lastId = 0
+        let filteredRecipes = []
+        recipes.forEach(recipe => {
+
+            if (recipe.id !== lastId)
+                filteredRecipes.push(recipe)
+            
+            if (recipe.path != null)
+                recipe.src = `${req.protocol}://${req.headers.host}${recipe.path.replace('public', '')}`
+
+            lastId = recipe.id
+        })
+
         return res.render('main/index', { recipes })
-    })
-}
+    },
 
-exports.about = (req, res) => {
-    return res.render('main/about')
-}
+    about(req, res) {
+        return res.render('main/about')
+    },
 
-exports.recipes = (req, res) => {
-    Recipe.selectAllWithChefNames((recipes) => {
+    async recipes(req, res) {
+        let results = await Recipe.selectAllWithChefNamesAndFiles()
+        const recipes = results.rows
+
         return res.render('main/recipes', { recipes })
-    })
-}
+    },
 
-exports.recipe = (req, res) => {
-    const { id } = req.params
+    recipe(req, res) {
+        const { id } = req.params
+
+        Recipe.find(id, (recipe) => {
+            return res.render("main/recipe", { recipe })
+        })
+    },
+
+    search(req, res) {
+        const { filter } = req.query
     
-    Recipe.find(id, (recipe) => {
-        return res.render("main/recipe", { recipe })
-    })
-}
+        Recipe.findBy(filter, (filteredRecipes) => {
+            res.render('main/search', { recipes: filteredRecipes, filter })
+        })
+    },
 
-exports.search = (req, res) => {
-    const { filter } = req.query
-
-    Recipe.findBy(filter, (filteredRecipes) => {
-        res.render('main/search', { recipes: filteredRecipes, filter })
-    })
-}
-
-exports.chefs = (req, res) => {
-    Chef.selectChefsWithTotalRecipes((chefs) => {
-        res.render('main/chefs', {chefs})
-    })
+    chefs(req, res) {
+        Chef.selectChefsWithTotalRecipes((chefs) => {
+            res.render('main/chefs', { chefs })
+        })
+    }
 }
