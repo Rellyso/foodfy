b0f7ef046eef13e221555ce217af037b1558bb4d
Rellyson <rellysondouglas2015@gmail.com>
2020-09-19 16:53:07 -0300

    Added remove image function on put queries

--- src/app/controllers/recipes.js
+++ src/app/controllers/recipes.js
@@ -10,7 +10,7 @@ module.exports = {
         let filteredRecipes = []
         recipes.forEach(recipe => {
 
-            
+
             if (recipe.id !== lastId) {
                 filteredRecipes.push(recipe)
             }
@@ -20,7 +20,7 @@ module.exports = {
 
             lastId = recipe.id
         })
-        
+
         return res.render('admin/recipes/index', { recipes: filteredRecipes })
 
     },
@@ -94,7 +94,7 @@ module.exports = {
                 file.src = `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
             })
 
-            return res.render(`admin/recipes/show`, {recipe, files})
+            return res.render(`admin/recipes/show`, { recipe, files })
         } catch (err) {
             res.send(err)
         }
@@ -115,55 +115,59 @@ module.exports = {
         files.map(file => {
             file.src = `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
         })
-        
+
         res.render('admin/recipes/edit', { recipe, options, files })
     },
 
     async put(req, res) {
         // teste de campos preenchidos
-        const keys = new Object.keys(req.body)
-        for (let key of keys) {
-            if (req.body[key] == "" && key != "removed_files") {
-                return res.send('Please fill in all the fields.')
-            }
-        }
-        
-        // teste de envio de pelo menos uma imagem
-        if (req.files.length == 0) {
-            return res.send("Please send at least one image.")
-        }
-
-
-        const { ingredients, preparation } = req.body
-        let newIngredients = [],
-            newPreparation = []
-
-        ingredients.map(ingredient => {
-            if (ingredient != "" || ingredient != 0)
-                newIngredients.push(ingredient)
-        })
-
-        preparation.map(prep => {
-            if (prep != "" || prep != 0)
-                newPreparation.push(prep)
-        })
-
-
-        const data = {
-            ...req.body,
-            id: parseInt(req.body.id, 10),
-            chef_id: parseInt(req.body.chef_id, 10),
-            ingredients: newIngredients,
-            preparation: newPreparation,
-        }
-
-        if (req.files == 0) {
-            res.send('Please send at least an image.')
-        }
-
-        // criar lógica de removed_files para excluir arquivos de imagem do files
-
         try {
+            const keys = Object.keys(req.body)
+            for (let key of keys) {
+                if (req.body[key] == "" && key != "removed_files") {
+                    return res.send('Please fill in all the fields.')
+                }
+            }
+
+            const { ingredients, preparation } = req.body
+            let newIngredients = [],
+                newPreparation = []
+
+            ingredients.map(ingredient => {
+                if (ingredient != "" || ingredient != 0)
+                    newIngredients.push(ingredient)
+            })
+
+            preparation.map(prep => {
+                if (prep != "" || prep != 0)
+                    newPreparation.push(prep)
+            })
+
+            const data = {
+                ...req.body,
+                id: parseInt(req.body.id, 10),
+                chef_id: parseInt(req.body.chef_id, 10),
+                ingredients: newIngredients,
+                preparation: newPreparation,
+            }
+
+            if (req.files.length != 0) {
+                const newFilesPromise = req.files.map(file => File.create({ ...file, recipe_id: req.body.id }))
+
+                await Promise.all(newFilesPromise)
+            }
+
+            if (req.body.removed_files) {
+                // 1, 2, 3,
+                const removedFiles = req.body.removed_files.split(',') // [1, 2, 3, ""]
+                const lastIndex = removedFiles.length - 1 // [1, 2, 3, -> ""]
+                removedFiles.splice(lastIndex, 1) // [1, 2, 3]
+
+                const removedFilesPromise = removedFiles.map(id => File.deleteFileFromRecipe(id))
+                
+                await Promise.all(removedFilesPromise)
+            }
+
             let results = await Recipe.update(data)
             const recipeId = results.rows[0].id
 
