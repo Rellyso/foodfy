019244424e704c16f39ac25a7fe8bcca95da4ce6
Rellyson <rellysondouglas2015@gmail.com>
2020-10-27 14:26:57 -0300

    getting post on login

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -3,4 +3,9 @@ module.exports = {
     async loginForm(req, res) {
         return res.render('session/login')
     },
+
+    async login(req, res) {
+        req.session.userId = req.users
+        return res.render('session/login', {error: 'Ainda não tá pronto'})
+    },
 }
