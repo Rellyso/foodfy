b21b80e4bc2aca739b28c9c4e5431729b8c790f7
Rellyson <rellysondouglas2015@gmail.com>
2020-11-03 20:13:34 -0300

    adding User model | Getting func to find one user

---
+++ src/app/models/User.js
@@ -0,0 +1,30 @@
+const db = require('../../config/db')
+
+module.exports = {
+    async findOne(filters) {
+        let query = `
+            SELECT * FROM users
+        `
+
+        Object.keys(filters).map(key => {
+            query = `${query}
+            ${key}
+            `
+            // SELECT * FROM users WHERE
+
+            Object.keys(filters[key]).map(field => {
+                // field -> name: email, value: example@mail.com
+                query = `${query} ${field} = '${filters[key][field]}'`
+                // SELECT * FROM users WHERE email = example@mail.com
+            })
+        })
+
+        try {
+            const results = await db.query(query)
+
+            return results.rows[0] || null
+        } catch (err) {
+            console.error(err)
+        }
+    },
+}
