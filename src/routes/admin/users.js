fe60998c16c8682c1a3f94fbc6f0926160693836
Rellyson <rellysondouglas2015@gmail.com>
2020-11-17 19:11:55 -0300

    updating routes | reparting Profile and User

--- src/routes/admin/users.js
+++ src/routes/admin/users.js
@@ -2,22 +2,25 @@ const express = require('express')
 const routes = express.Router()
 
 const ProfileController = require('../../app/controllers/ProfileController')
+const UserController = require('../../app/controllers/UserController')
 const UserValidator = require('../../app/validators/user')
+const ProfileValidator = require('../../app/validators/profile')
 
 // rotas de sessão
 
 // // rotas de usuário logado
-routes.get('/profile', UserValidator.show, ProfileController.index)
-routes.put('/profile', UserValidator.update, ProfileController.put)
+routes.get('/profile', ProfileValidator.show, ProfileController.index)
+routes.put('/profile', ProfileValidator.update, ProfileController.put)
 
 
 // // Rotas para gerenciamento dos administradores
-routes.get('/users', ProfileController.list)
-routes.get('/users/create', ProfileController.create)
+routes.get('/users', UserController.list)
+routes.get('/users/create', UserController.create)
+routes.get('/users/edit/:id', UserValidator.edit, UserController.edit)
 
-routes.post('/users/create', UserValidator.post, ProfileController.post)
-// routes.put('/users', ProfileController.put)
-// routes.delete('/users', ProfileController.delete)
+routes.post('/users/create', UserValidator.post, UserController.post)
+routes.put('/users', UserValidator.update, UserController.put)
+// routes.delete('/users', UserController.delete)
 
 
 module.exports = routes
