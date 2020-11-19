293b605104ef50ec79d9f6f2b0b4dee0e43be5da
Rellyson <rellysondouglas2015@gmail.com>
2020-11-18 22:24:01 -0300

    Updated routes

--- src/routes/session.js
+++ src/routes/session.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const routes = express.Router()
 
-const ProfileController = require('../app/controllers/ProfileController')
 const SessionController = require('../app/controllers/SessionController')
 const SessionValidator = require('../app/validators/session')
 // rotas de sessão
@@ -12,10 +11,10 @@ routes.post('/login', SessionValidator.login, SessionController.login)
 routes.post('/logout', SessionController.logout)
 
 // // password reset/forgot password
-// routes.get('/password-reset', SessionController.resetForm)
-// routes.get('/forgot-password', SessionController.forgotForm)
+routes.get('/password-reset', SessionController.resetForm)
+routes.get('/forgot-password', SessionController.forgotForm)
 // routes.post('/password-reset', SessionController.reset)
-// routes.post('/forgot-password', SessionController.forgot)
+routes.post('/forgot-password', SessionValidator.forgot, SessionController.forgot)
 
 
 module.exports = routes
