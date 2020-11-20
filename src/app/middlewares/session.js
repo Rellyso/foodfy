fdd035501417755b1c3e0e71e13a5a772981e442
Rellyson <rellysondouglas2015@gmail.com>
2020-11-20 12:15:08 -0300

    Added function to filter admin users on routes

--- src/app/middlewares/session.js
+++ src/app/middlewares/session.js
@@ -1,3 +1,5 @@
+const User = require('../models/User')
+
 function onlyUsers(req, res, next) {
     if (!req.session.userId)
         return res.redirect('/accounts')
@@ -5,6 +7,28 @@ function onlyUsers(req, res, next) {
     next()
 }
 
+async function onlyAdmins(req, res, next) {
+    try {
+        const { userId } = req.session
+
+        const user = await User.findOne(userId)
+
+        if (!user.is_admin) {
+            return res.render('admin/profile/index', {
+                user: user,
+                error: 'Somente admins podem acessar.'
+            })
+        }
+
+        next()
+    } catch (err) {
+        return res.render('session/login', {
+            error: 'Você precisa se conectar para acessar.'
+        })
+    }
+}
+
 module.exports = {
-    onlyUsers
+    onlyUsers,
+    onlyAdmins,
 }
